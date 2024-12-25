b0dbec64869bbb97d329008c3edab36681754b70
ffffish-B <bhyf0401@163.com>
2024-12-25 20:58:09 +0800

    我真忘了debug的时候要提交

---
+++ HW6/TriShapeFunction_Grad.m
@@ -0,0 +1,12 @@
+function [N_xi, N_eta] = TriShapeFunction_Grad(node, xi, eta)
+    if node == 1
+        N_xi=-1;
+        N_eta=-1;
+    elseif node == 2
+        N_xi = 1;
+        N_eta = 0;
+    elseif node == 3
+        N_xi = 0;
+        N_eta = 1;
+    end
+end
